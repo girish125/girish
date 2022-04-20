b3eacaf8545010a814f4737be7858cc807bc484c
girish125 <104081679+girish125@users.noreply.github.com>
2022-04-20 20:53:50 +0530

    hello

---
+++ inroduction.py
@@ -0,0 +1,999 @@
+#
+# print("hello world")
+# x=100
+# y=200
+# b="girish"
+# print(type(x))
+# print(type(b))
+# print("x+y=",x+y)
+# # help('keywords')
+# print("{} and {}".format(x,y))
+# print("{0} and {1}".format(x,y))
+# print("{x} and {y}".format(x=500,y=200))
+# print(f"{x} and {y}")
+# str_1 ='this is single quote'+'\'s'
+# print(str_1)
+# s='python'
+# print(s[5])
+# print(len(s))
+# # print(s(len(s)-1))
+# d="foo bar girish"
+# print(d.endswith('h'))
+# print(dir(d))
+# print(d.capitalize())
+# print(d.lower())
+# print(d.count('o',0,5))
+# print(d[-2:])
+# print(d[2:2]) # empty
+# print(d[4:2])
+# h='!'
+# print(ord(h))
+# print(chr(97))
+# print(ord('+'))
+# s=(1,23,45)
+# print(type(s))
+# print(str(s))
+# print(type(s))
+# print(u'\U0001F500')
+# r="muni girish"
+# print(r.replace('m','D'))
+# r="D"+r[1:]
+# # print(s.find('m'))
+# print(r)
+# print(len(s))
+# print(r.strip())
+# print(len(s))
+# s="python"
+# str_1="j"+s[1:]
+# print(str_1)
+# print(s.replace('p','j'))
+# print(s)
+# s="munigirish"
+# print(' '.join(s))
+# txt="i am new to python programming language"
+# print(txt)
+# print(txt.splitlines(True))
+# print(txt.splitlines(False))
+# print(txt.partition("muni"))
+# print(ord(h))
+# import string
+# print(string.printable)
+# print(string.ascii_uppercase)
+# print(string.ascii_lowercase)
+# print(string.whitespace)
+# print(string.digits)
+# import random
+# # def generated_password(size_of_password,charaters=ascii_uppercase):
+# # list=[1,22,36,566,56629,656562,55,'muni','girish',25,125,8801838724]
+# list_4=['giri','muni','dan']
+# list_3=[]
+# print(type(list_3))
+# # for l in list:
+# #     print(l)
+# str="munigirish"
+# print(str[::-4])
+# print(str[4])
+# print(str[1:100])
+# list=['foo','bar','qux','bcv']
+# # list.remove('girish')
+# print(list)
+# list.sort()
+# print(list)
+#
+# d=list.copy()
+# print(d)
+# print(list.pop(0))
+# list.clear()
+# print(list)
+# # list.remove('girish')
+# print(list)
+# print(list)
+# l=list.append("python")
+# print(l)
+# print(list.append("python"))
+# list[-1]='girish'
+# print(list)
+# l1=[1,2,3,4]
+# l2=[4,5,1,6]
+# del l1[2]
+# print(l1)
+# print(l1 * 5)
+#
+# name = "Dandakatla Muni girish"
+# language= "python"
+# print(f"{name} is learning {language} course")
+# print("{0} is learning {1} course".format(name,language))
+# print("{name} is learning {language} course".format(name='girish',language='java'))
+# to find a sqaure root of a number
+# number = int(input('enter a number :'))
+# square_root= number**2
+# print(f"sqaure root of {number} is :",square_root)
+# program for swaping of two numbers
+# number_1=int(input("enter first number :"))
+# number_2=int(input("enter second number :"))
+# swaped_numbers=number_1,number_2=number_2,number_1
+# print("after swapping of two number is :",(swaped_numbers))
+# how to print a random number program
+# import random
+# print(random.randrange(1,1000))
+# write a program for weather a given number is positive or negative
+# number=int(input("enter a number :"))
+# if number<0:
+#     print(f"{number} number is neative")
+# else:
+#     print(f"{number} number is postive")
+# write a program for weather a given number is even or odd
+# number=int(input("enter a number :"))
+# if number%2==0:
+#     print(f"{number} is EVEN number")
+# else:
+#     print(f"{number} is ODD number ")
+# write a program for weather a given year is leap year or not
+# year=int(input("enter an year :"))
+# if (year % 400 == 0) and (year % 100 == 0):
+#     print(f"{year} is leap year")
+# elif (year % 4 == 0) and (year % 100 !=0):
+#     print(f"{year} is a leap year")
+# else:
+#     print(f"{year} is not a leap year ")
+# write a program for to find a largest number among given numbers
+# number_1=int(input("enter first number :"))
+# number_2=int(input("enter second number :"))
+# number_3=int(input("enter third number :"))
+# if number_1>number_2 and number_1>number_3:
+#     print(f"{number_1} number is greater ")
+# elif number_2>number_1 and number_2>number_3:
+#     print(f"{number_2} number is greater ")
+# else:
+#     print(f"{number_3} number is greater ")
+# Python program to display all the prime numbers within an interval
+#
+# lower = int(input("enter a lower value :"))
+# upper = int(input("enter a upper value :"))
+#
+# print("Prime numbers between {a} and {b} are:".format(a=lower,b=upper))
+#
+# for num in range(lower, upper + 1):
+#    # all prime numbers are greater than 1
+#    if num > 1:
+#        for i in range(2, num):
+#            if (num % i) == 0:
+#                break
+#        else:
+#            print(num)
+# write a program for a multiplication table
+# num=int(input("enter a number :"))
+# for item in range(1,11):
+#     print(num,'X',item,'=' ,(num*item))
+# write a program for to find ASCII value of a given number
+# number=input("enter a character :")
+# print(f"The ASCII value of {number} is ",ord(number))
+# Program to display calendar of the given month and year
+# import calendar
+#
+# year  = int(input("Enter year: "))
+# month = int(input("Enter month: "))
+#
+# print(calendar.month(year,month))
+
+
+# import random
+# print(random.randrange(1,10000))
+# str= "hello"
+# print(str[0],str[4])
+# print(len(str ))
+# print(s.index('grish'))
+# print(s.startswith('foo'))
+# print(s.count('ww'))
+# print(s.upper())
+# print(s.lower())
+# print(s.title() )
+
+
+
+
+
+
+
+#
+# l1=[1,2,6,4]
+# l2=[4,5,1,6]
+# l1.sort(reverse=True)
+# print(l1)
+# t=(2,4,2,5,6,'giri','muni',6)
+# d=t.index(6)
+# print(d)
+# T=('red','YELLOW','Blue','orange','purple')
+# t1,*_,t3 =T
+# print(_)
+# s={'red','yellow','blue'}
+# # s=set('blue')
+# d=s.pop()
+# print(d)
+# x1={'foo','bar','baz'}
+# x2={'foo','baz'}
+# print(x2.issubset(x1))
+# print(x2<=x1)
+# print(x1.isdi sjoint(x2))
+# d=x1.union(x
+#
+# x1.intersection_update(x2)
+# print(x1)
+# print(x1.interse(x2))
+# # print(x1)
+# student_dict=dict(
+#     name='girish',
+#     age= 26,
+#     job='python')
+# student_dict['salary']=9000
+# print(student_dict)
+# a={'a': 5,'b':10,'c':25}
+# b={'b':200,'d':400}
+# c=(['d',100],['e',25])
+# a.update(d=2500,a=135894595256)
+# print(a)
+# str="oh! yes, this is awsome."
+# if '!' in str:
+#     print("puncutations are present in str")
+# name=' DANDAKATLA munigirish'
+# while name:
+#     print(name)
+#     name=name[:-1 ]
+# for i in range(1,10):
+#     if i%2!=0:
+#         continue
+#     print(i)
+# while 10:
+#     pass
+# num='hello world'
+# for i in num:
+# print("2856526")
+# L=['red','blue','green',1,4,8,9,5,5656,89,"girish","Muni"]
+# for index,value in enumerate(L):
+#     print("{} is at position {}".format(value,index))
+# for item in range(1,10):
+#     print(item)
+# l=[1.5,6,64,7,265,65]
+# l.append(62)
+# print(l)
+# Python code to reverse a string
+# using loop
+
+# def reverse(s):
+#     name = ""
+#     for i in s:
+#         name = i + name
+#         return name
+#
+# s = input('enter a string :')
+#
+# print ("The original string is : ",s)
+# # print (s)
+#
+# print ("The reversed string(using loops) is : ",reverse(s))
+# # print (reverse(s))
+# name=input('enter a string :')
+# str=""
+# for ch in name:
+#     ch -= str
+#     print(str)
+# Python code to reverse a string
+# using loop
+
+# def reverse(s):
+#     str = ""
+#     for i in s:
+# 	    str = i + str
+#     return str
+#
+# s = input("enter a string :")
+#
+# print ("The original string is : ",end="")
+# print (s)
+#
+# print ("The reversed string(using loops) is : ",end="")
+# print (reverse(s))
+# def reverse_string(str):
+#     str1 = ""  # Declaring empty string to store the reversed string
+#     for i in str:
+#         str1 = i + str1
+#     return str1  # It will return the reverse string to the caller function
+#
+#
+# str = input("enter a string :")  # Given String
+# print("The original string is: ", str)
+# print("The reverse string is: ", reverse_string(str))  # Function call
+# def cal(x,y):
+#     x=int(input("enter x value :"))
+#     y = int(input("enter x value :"))
+#     add=x+y
+#     sub=x-y
+#     mul=x*y
+#     div=x%y
+#     return add,sub,mul,div
+# print(cal())
+# def length(iterable):
+#     count=0
+#     for ch in iterable:
+#         count+=1
+#     return count
+# print(length("munigirish"))
+# def hello(name):
+#     print(f"hello {name}")
+#
+# hello('girish')
+# hello("munigiirsh")
+# lower_range=int(input("enter lower value:"))
+# upper_range=int(input("enter upper value:"))
+# def prime_number_list(lower_range,upper_range):
+#     prime_numbers=[]
+#     for number in range(lower_range,upper_range+1):
+#         if number >1:
+#             for i in range(2,number):
+#                 if number%i==0:
+#                     break
+#             else:
+#                 prime_numbers.append(number)
+#     return prime_numbers
+#
+# print(prime_number_list(lower_range,upper_range))
+
+# for number in range(1,101):
+#     if number >1:
+#         for i in range(2,number):
+#             if number%i==0:
+#                 break
+#         else:
+#             print(number)
+# def reverse_string(input_string):
+#     reversed_string =""
+#     index=len(input_string)
+#     while index:
+#         index-=1
+#         reversed_string+=input_string[index]
+#     return reversed_string
+# print(reverse_string("muni" ))
+# def name(**names):
+#     print(names)
+# name(a='girish',b='muni')
+# def outer (a,b):
+#     def inner (c,d):
+#         return c+d
+#     return inner(a,b)
+# print(outer(4,5))
+# def countdown(num):
+#     if num<=0:
+#         print("stop")
+#     else:
+#         print(num)
+#         countdown(num-1)
+#
+# countdown(995)
+# x=250
+# print(x)
+# def sample_function():
+#     x=100
+#     print(x)
+#     print(locals())
+# sample_function()
+# print(x)
+# print(globals())
+# print(locals())
+# l1=[1,2,3,4,5]
+# l2=[6,7,8,9,10]
+# for a,b in zip(l1,l2):
+#     new_list=[]
+#     zip.append
+# def outer_func():
+#     def inner_func():
+#         print("inside the inner function")
+#     return inner_func()
+#
+# outer_func()
+# def stars(func):
+#     def inner():
+#         print("#"*50)
+#         func()
+#         print("#"*50)
+#     return inner
+#
+# @stars
+#
+# def stars_func():
+#     print("this message is decorated with stars")
+#
+# stars_func()
+
+# from datetime import datetime
+# print(datetime.now())
+# names=['giri','munni','dandakatla']
+# # l=names.__iter__()
+# print(names.__next__())
+
+
+class student:
+    def __inti__(self,f_name,l_name,courses):
+        self.first_name=f_name
+        self.last_name=l_name
+        self.courses=courses
+    def add_course(self,courses):
+        if courses not in self.courses:
+            self.courses.append(courses)
+        else:
+            print(f"{self.first_name} you're already enrolled in this {courses}")
+
+
+# class person:
+#     def __init__(self,name,desgination):
+#         print("from parson class")
+#         self.name=name
+#         self.designation=desgination
+#
+#     def learn(self):
+#         print("person will learn")
+#
+#     def walk(self):
+#         print("person will walk")
+#
+#     def eat(self):
+#         print("person will eat")
+#
+# class programmer(person):
+#     def __init__(self,p_name,p_desgination,company):
+#         print("from programmer class")
+#         person.__init__(self,name=p_name,desgination=p_desgination)
+#         self.company=company
+#     def coding(self):
+#         print("person will do coding also")
+#
+# programmer_obj=programmer(p_name="girish",p_desgination="python tester",company="dell")
+# print(programmer_obj.name,programmer_obj.company,programmer_obj.designation)
+# programmer_obj.learn()
+# programmer_obj.walk()
+# programmer_obj.eat()
+# programmer_obj.coding()
+
+# class A:
+#     def show(self):
+#         return "from class A"
+#
+# class B(A):
+#     def display(self):
+#         return "from class B"
+#
+# obj=A()
+# print(obj.show())
+
+# class A:
+#     def show(self):
+#         return "from class A"
+#
+# class B():
+#     def display(self):
+#         return "from class B"
+#
+# class C(A,B):
+#     def print_method(self):
+#         return "from class C"
+#
+# obj=B()
+# print(obj.show())
+# print(obj.display())
+# print(obj.print_method())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
